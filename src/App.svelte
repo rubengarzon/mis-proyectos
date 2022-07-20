71fb86053fb2d6aeb0901bd4585c8e9c9ecbbdf9
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-20 11:42:17 +0200

    componente proyecto creado

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Proyecto from "./lib/Proyecto.svelte";
+</script>
+
+<h1>Mis proyectos</h1>
+<h2>Rubén Garzón</h2>
+<div>
+  <h3>Angular</h3>
+  <Proyecto />
+</div>
---
+++ src/lib/Proyecto.svelte
@@ -0,0 +1,17 @@
+<div>
+  <img src="../public/cineencasa.PNG" alt="aa" width="500" />
+  <br />
+  <span>Cineencasa</span>
+  <ul>
+    <li>Muestra las peliculas y series populares</li>
+    <li>Inicio de sesión</li>
+    <li>Perfil de usuario personalizable</li>
+    <li>Lista con las peliculas y series que el usuario ha dado like</li>
+  </ul>
+</div>
+
+<style>
+  img {
+    border-radius: 10px;
+  }
+</style>
